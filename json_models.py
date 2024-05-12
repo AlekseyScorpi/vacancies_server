8df7d9f49a369bf45b520787b6bf6ea6a3c8c99e
AlekseyScorpi <timoshin_aleksey02@mail.ru>
2024-05-12 19:48:08 +0300

    fix check-answer bug, add more docs for all files

--- json_models.py
+++ json_models.py
@@ -2,8 +2,13 @@ from typing import List
 from pydantic import BaseModel, Field
 
 class RequestGenerateVacancy(BaseModel):
+    """Class for form request validation to create new generation task
+
+    Args:
+        BaseModel (_type_): default pydantic basemodel
+    """
     vacancy_name: str = Field(alias='vacancyName')
-    company_name: str = Field(alias='cityFullName', default="")
+    company_name: str = Field(alias='companyName', default="")
     company_place: str = Field(alias='companyPlace', default="")
     schedule: str = ""
     experience: str = ""
@@ -11,4 +16,9 @@ class RequestGenerateVacancy(BaseModel):
     token: str
     
 class RequestCheckStatus(BaseModel):
+    """Class for check-answer request validation to check current task status
+
+    Args:
+        BaseModel (_type_): default pydantic basemodel
+    """
     token: str
--- server.py
+++ server.py
@@ -11,13 +11,16 @@ import time
 import dotenv
 import os
 
-# env var load
+# environment variables load
 dotenv.load_dotenv()
+# port for flask server
 port = os.getenv('FLASK_PORT')
+# client domain (need to CORS policy)
 client_domain = os.getenv('CLIENT_DOMAIN')
 
 # app instance
 app = Flask(__name__)
+# set CORS policy
 CORS(app, origins=[client_domain if client_domain else "/", "http://localhost:3000"])
 
 # config instance
@@ -33,18 +36,26 @@ config = ModelConfig(
 # model instance
 model = Model(config)
 
+# queue for waiting tasks
 task_queue: List[RequestGenerateVacancy] = []
+# tasks which is currently processing by model
 processing_tasks: Set[str] = set()
+# result pool
 result_pool: Dict[str, Dict[str, str | float]] = {}
+# mutex for tasks queue
 queue_lock = threading.Lock()
+# mutex for processing tasks
 processing_lock = threading.Lock()
+# mutex for result pool
 result_lock = threading.Lock()
 
+# time, before the task is removed from the result pool if it is not requested to receive 
 MAX_CACHE_TIME = 60
 
 
-
 def process_task():
+    """Main function to manage tasks
+    """
     while True:
         with result_lock:
             del_tokens=[]
@@ -58,9 +69,12 @@ def process_task():
             task = task_queue.pop(0) if task_queue else None
         
         if task:
-            result = process_task_logic(task)
-            with result_lock:
-                result_pool[task.token] = {'content': result, 'timestamp': time.time()}
+            try:
+                result = process_task_logic(task)
+                with result_lock:
+                    result_pool[task.token] = {'content': result, 'timestamp': time.time()}
+            except(RuntimeError):
+                print(f"{'%Y-%m-%d %H:%M:%S', time.localtime()} Task by token={task.token} cannot be completed, unexpected error occured")
             with processing_lock:
                 processing_tasks.remove(task.token)
         else:
@@ -68,10 +82,22 @@ def process_task():
             
 
 def process_task_logic(task: RequestGenerateVacancy) -> str:
+    """This function send request to the model and return model request output
+
+    Args:
+        task (RequestGenerateVacancy): Request from user
+
+    Raises:
+        RuntimeError: raise RuntimeError if RuntimeError from model occured
+
+    Returns:
+        str: generated vacancy text
+    """
     with processing_lock:
         processing_tasks.add(task.token)
         
-    answer: str = model.generate(
+    try:
+        answer: str = model.generate(
         vacancy_name=task.vacancy_name,
         company_name=task.company_name,
         company_place=task.company_place,
@@ -79,10 +105,17 @@ def process_task_logic(task: RequestGenerateVacancy) -> str:
         experience=task.experience,
         key_skills=task.key_skills,
     )
+    except(RuntimeError):
+        raise RuntimeError("Model generation error occured")
     return answer
 
 @app.route("/api/check-answer", methods=['POST'])
 def check_answer_status():
+    """Flask route function. Using for check task status and return position or position/vacancy_text to client
+
+    Returns:
+        json: json which contains message and content fields
+    """
     try:
         data = request.json
         
@@ -95,7 +128,7 @@ def check_answer_status():
         
         with result_lock:
             if token in result_pool:
-                answer = result_pool[token]
+                answer = result_pool[token]['content']
                 response = {'message': 'GOOD', 'content': answer}
                 del result_pool[token]
                 return jsonify(response), 200
@@ -123,6 +156,11 @@ def check_answer_status():
 
 @app.route("/api/generate", methods=['POST'])
 def create_task():
+    """Flask route function. Using for create new task based on user form request
+
+    Returns:
+        json: return json, which contains user request status
+    """
     try:
         data = request.json
 
@@ -140,20 +178,18 @@ def create_task():
     except ValidationError as e:
         error_response = {'message': 'BAD', 'content': 'Invalid JSON format: ' + str(e)}
         return jsonify(error_response), 400
-
-
-@app.route("/api/home", methods=['GET'])
-def return_home():
-    return jsonify({
-        'description': str(model._model)
-    })
+    
 
 def start_task_processing():
+    """Function to start new thread for tasks manage
+    """
     global thread
     thread = threading.Thread(target=process_task)
     thread.daemon = True
     thread.start()
 
 if __name__ == "__main__":
+    """Start flask server
+    """
     start_task_processing()
     app.run(port=int(port)) if port else app.run(port=80)
